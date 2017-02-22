e745e2a34bcba248f835060946818012622bf06e
sakooh <sakooh.tent@gmail.com>
2017-02-22 23:19:31 +0700

    Add files via upload

---
+++ required/menu_bar.php
@@ -0,0 +1,7 @@
+<div class="menu_bar">
+	<ul>
+		<a href="index.php"><li><i class="fa m2 fa-home fa-1x"></i></li></a>
+		<a href="promotion.php"><li><i class="fa fa-barcode fa-1x"></i></li></a>
+		<a href="logout.php"><li><i class="fa fa-sign-out fa-1x"></i></li></a>
+	</ul>
+</div>
